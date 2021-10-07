cccd27d11a4c67c90bead3b7d9412ee7e2cdf01b
Jasmine Vang <jvang038@student.butte.edu>
2021-10-07 17:59:51 +0000

    Adding Lab

--- labs/12/lab_12.cpp
+++ labs/12/lab_12.cpp
@@ -1,6 +1,6 @@
 /*
  * Name        : lab_12.cpp
- * Author      : FILL IN
+ * Author      : Jasmine Vang
  * Description : Working with Pointers and Dynamic Variables / Arrays
  */
 #include <iostream>
@@ -26,6 +26,7 @@ int* MakeDynoIntArray(unsigned int size);
  *        Syntax: throw "The Message to throw";
  */
 int Sum(int* the_array, unsigned int array_size);
+
 
 /*
  * Identify the max value in an array.
@@ -66,7 +67,77 @@ int main() {
 }
 
 // CODE HERE -- FUNCTION DEFINITIONS
+/*
+ * Allocate memory for a dynamic array of integers.
+ * @param unsigned int size - The desired size of the dynamic array
+ * @return int* - A pointer to the newly allocated integer array
+ */
+int* MakeDynoIntArray(unsigned int size) {
+  int *array;
+  array = new int[size];
+  return array;
+}
 
+/*
+ * Compute the sum of an array.
+ * @param int* the_array - The array for which the sum will be computed
+ * @param unsigned int array_size - The size of the_array
+ * @return int - An integer containing the sum of the array
+ * @throw The message "NULL ARRAY REFERENCE" if the_array is NULL
+ *        Syntax: throw "The Message to throw";
+ */
+int Sum(int* the_array, unsigned int array_size) {
+  if (the_array == NULL) {
+    throw "NULL ARRAY REFERENCE";
+  }
+  int sum = 0;
+  for (unsigned int i = 0; i < array_size; i++) {
+    sum = sum + the_array[i];
+  }
+  return sum;
+}
+
+/*
+ * Identify the max value in an array.
+ * @param int* the_array - The array for which the max value will be identified
+ * @param unsigned int array_size - The size of the_array
+ * @return int - An integer containing the max value of the array
+ * @throw The message "NULL ARRAY REFERENCE" if the_array is NULL
+ *        Syntax: throw "The Message to throw";
+ */
+int Max(int* the_array, unsigned int array_size) {
+  if (the_array == NULL) {
+    throw "NULL ARRAY REFERENCE";
+  }
+  int max = the_array[0];
+  for (unsigned int i = 0; i < array_size; i++) {
+    if (the_array[i] > max) {
+      max = the_array[i];
+    }
+  }
+  return max;
+}
+
+/*
+ * Identify the min value in an array.
+ * @param int* the_array - The array for which the min value will be identified
+ * @param unsigned int array_size - The size of the_array
+ * @return int - An integer containing the min value of the array
+ * @throw The message "NULL ARRAY REFERENCE" if the_array is NULL
+ *        Syntax: throw "The Message to throw";
+ */
+int Min(int* the_array, unsigned int array_size) {
+  if (the_array == NULL) {
+    throw "NULL ARRAY REFERENCE";
+  }
+  int min = the_array[0];
+  for (unsigned int i = 0; i < array_size; i++) {
+    if (the_array[i] < min) {
+      min = the_array[i];
+    }
+  }
+  return min;
+}
 
 // For testing (DO NOT ALTER)
 void UnitTest() {
